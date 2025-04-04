a03ffbfb23e313ceb9f442bc6d1a8cc48242c890
ahmed-elgihamy <ahmedelghmay9781@gmail.com>
2025-04-04 18:25:33 +0200

    f

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        main: ["Raleway", "sans-serif"],
+      },
+      backgroundImage: {
+        hero: "url('/src/assets/hero.jfif')",
+        button: "url('/src/assets/button.jfif')",
+        banner: "url('/src/assets/banner.jfif')",
+      },
+    },
+  },
+  plugins: [],
+}
+
+
